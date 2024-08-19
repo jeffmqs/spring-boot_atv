44151bd6a2e543b6595d02ff0a3e7dc59be18c99
Jeff Marques <129700391+jeffmqs@users.noreply.github.com>
2024-08-18 22:18:34 -0300

    Add arquivos

---
+++ previsao-tempo/previsao-tempo/src/main/java/react_deploy/previsao_tempo/PrevisaoTempoApplication.java
@@ -0,0 +1,13 @@
+package react_deploy.previsao_tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisaoTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevisaoTempoApplication.class, args);
+	}
+
+}
---
+++ previsao-tempo/previsao-tempo/src/test/java/react_deploy/previsao_tempo/PrevisaoTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package react_deploy.previsao_tempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisaoTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
